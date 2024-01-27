ca3fa384f46bd28bf1815e97b980b5762c89df12
haitem <silentislp@gmail.com>
2024-01-27 07:50:52 +0100

    Make better player and start enemy

--- CatGame/Scenes/Player/player.gd
+++ CatGame/Scenes/Player/player.gd
@@ -3,23 +3,30 @@ extends CharacterBody2D
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = 400.0
 
+@onready var fallparticle : CPUParticles2D = get_node("CPUParticles2D")
+
 var isJumping = false
 var isCharging = false
 var jumpCoeficient = 0
+var fallDuration : int
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
+	fallDuration += 1
 	if not is_on_floor():
 		velocity.y += (gravity * delta)
 	else:
+		if fallDuration > 70:
+			fallparticle.emitting = true
+		fallDuration = 0
 		isJumping = false
 
 	# Handle jump.	
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		$AnimatedSprite2D.play("charge")		
+		$AnimatedSprite2D.play("charge")
 		isCharging = true
 		
 	if Input.is_action_pressed("Jump") and is_on_floor() and isCharging:
