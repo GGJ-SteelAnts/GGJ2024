b08f74fb2742bc53e351f0885d85929e3cef7fc2
haitem <silentislp@gmail.com>
2024-01-27 08:17:46 +0100

    Update enemy

--- CatGame/Scenes/Enemy/enemy.gd
+++ CatGame/Scenes/Enemy/enemy.gd
@@ -1,2 +1,27 @@
 extends CharacterBody2D
 
+@export var speed = 100.0
+@export var anger = 200.0
+
+@onready var animator = get_node("AnimatedSprite2D")
+
+var state = "Idle"
+var actualAnger : float
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	actualAnger = anger
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += (gravity * delta)
+	
+	move(delta)
+	animation()
+
+func animation():
+	animator.play(state)
+
+func move(delta):
+	velocity.x = move_toward(velocity.x, 0, speed)
+	move_and_slide()
