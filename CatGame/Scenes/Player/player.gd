ac6b586bde616a6f9f5c0b2a087818fe0f353c52
JonatanRek <vasek@steelants.cz>
2024-01-27 14:22:54 +0100

    Setable Jump LIMIT

--- CatGame/Scenes/Player/player.gd
+++ CatGame/Scenes/Player/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = 400.0
+@export var JUMP_LIMIT = 500
 
 @onready var fallparticle : CPUParticles2D = get_node("CPUParticles2D")
 
@@ -42,7 +43,7 @@ func _physics_process(delta):
 		$AnimatedSprite2D.play("charge")
 		isCharging = true
 		
-	if Input.is_action_pressed("Jump") and is_on_floor() and isCharging and jumpCoeficient <= 400:
+	if Input.is_action_pressed("Jump") and is_on_floor() and isCharging and jumpCoeficient <= JUMP_LIMIT:
 		jumpCoeficient = (jumpCoeficient + 2)
 		print(jumpCoeficient)
 		
