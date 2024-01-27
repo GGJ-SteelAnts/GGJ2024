808677065c542b8c615ffe227d7349be11d3182d
Martin Šustr <57565777+masu132@users.noreply.github.com>
2024-01-27 20:57:32 +0100

    working on tasks

--- CatGame/Scenes/Tasks/task.gd
+++ CatGame/Scenes/Tasks/task.gd
@@ -1,14 +1,18 @@
 extends Control
 
-var TaskType : TaskTypeEnum.TaskTypeEnum
+var TaskType : Enums.TaskTypeEnum = Enums.TaskTypeEnum.Empty
+var time : float = 0.0
+var amount : int = 0
+var item : Enums.ItemTypeEnum = Enums.ItemTypeEnum.Empty
+
 @onready var icon = get_node("TaskFrame/Icon")
 @onready var taskTimerBar = get_node("TaskTimerBar")
 @onready var label = get_node("Label")
 
-var HungryTexture = preload("res://Scenes/Assets/Tasks/Hungry.png")
-var SleepyTexture = preload("res://Scenes/Assets/Tasks/Sleepy.png")
-var CatnipyTexture = preload("res://Scenes/Assets/Tasks/Catnipy.png")
-var ShittyTexture = preload("res://Scenes/Assets/Tasks/Shitty.png")
+var BreakItemTexture = preload("res://Scenes/Assets/Tasks/Hungry.png")
+var DontBreakTexture = preload("res://Scenes/Assets/Tasks/Sleepy.png")
+var CatchMouseTexture = preload("res://Scenes/Assets/Tasks/Catnipy.png")
+var PottyPottyTexture = preload("res://Scenes/Assets/Tasks/Shitty.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,31 +23,92 @@ func _process(delta):
 	chooseArt(TaskType)
 	pass
 
+func copyResource(task):
+	TaskType = task.TaskType
+	time = task.time
+	amount = task.amount
+	item = task.item
+
+func resetResource():
+	TaskType = Enums.TaskTypeEnum.Empty
+	time = 0.0
+	amount = 0
+	item = Enums.ItemTypeEnum.Empty
+
 func chooseArt(task):
 	match task:
-		TaskTypeEnum.TaskTypeEnum.Hungry:
-			icon.texture = HungryTexture
-		TaskTypeEnum.TaskTypeEnum.Sleepy:
-			icon.texture = SleepyTexture
-		TaskTypeEnum.TaskTypeEnum.Catnipy:
-			icon.texture = CatnipyTexture
-		TaskTypeEnum.TaskTypeEnum.Shitty:
-			icon.texture = ShittyTexture
-		TaskTypeEnum.TaskTypeEnum.Empty:
+		Enums.TaskTypeEnum.BreakItem:
+			icon.texture = BreakItemTexture
+		Enums.TaskTypeEnum.DontBreak:
+			icon.texture = DontBreakTexture
+		Enums.TaskTypeEnum.CatchMouse:
+			icon.texture = CatchMouseTexture
+		Enums.TaskTypeEnum.PottyPotty:
+			icon.texture = PottyPottyTexture
+		Enums.TaskTypeEnum.Empty:
 			icon.texture = null
 			
 func chooseTask():
 	var rng = RandomNumberGenerator.new()
-	var random_number = rng.randi_range(1, TaskTypeEnum.TaskTypeEnum.size() - 1)
-	var taskString = TaskTypeEnum.TaskTypeEnum.keys()[random_number]
-	var task : TaskTypeEnum.TaskTypeEnum
+	var random_number = rng.randi_range(1, Enums.TaskTypeEnum.size() - 1)
+	var taskString = Enums.TaskTypeEnum.keys()[random_number]
+	var task : Enums.TaskTypeEnum
 	match taskString:
-		"Hungry":
-			task = TaskTypeEnum.TaskTypeEnum.Hungry
-		"Sleepy":
-			task = TaskTypeEnum.TaskTypeEnum.Sleepy
-		"Catnipy":
-			task = TaskTypeEnum.TaskTypeEnum.Catnipy
-		"Shitty":
-			task = TaskTypeEnum.TaskTypeEnum.Shitty
+		"BreakItem":
+			task = Enums.TaskTypeEnum.BreakItem
+		"DontBreak":
+			task = Enums.TaskTypeEnum.DontBreak
+		"CatchMouse":
+			task = Enums.TaskTypeEnum.CatchMouse
+		"PottyPotty":
+			task = Enums.TaskTypeEnum.PottyPotty
 	return task
+
+func generateTaskStats(taskType):
+	var rng = RandomNumberGenerator.new()
+	match taskType:
+		Enums.TaskTypeEnum.BreakItem:
+			time = rng.randf_range(20, 30)
+			var random_number = rng.randi_range(1, Enums.ItemTypeEnum.size() - 1)
+			var itemString = Enums.TaskTypeEnum.keys()[random_number]
+			match itemString:
+				"Vase":
+					item = Enums.ItemTypeEnum.Vase
+					amount = rng.randi_range(1, 3)
+				"Laptop":
+					item = Enums.ItemTypeEnum.Laptop
+					amount = rng.randi_range(1, 1)
+				"FishTank":
+					item = Enums.ItemTypeEnum.FishTank
+					amount = rng.randi_range(1, 1)
+				"Glass":
+					item = Enums.ItemTypeEnum.Glass
+					amount = rng.randi_range(1, 3)
+			label.text = str(amount) + "/" + str(amount)
+			
+		Enums.TaskTypeEnum.DontBreak:
+			time = rng.randf_range(20, 30)
+			item = Enums.ItemTypeEnum.Empty
+			amount = 0
+			label.text = str(amount) + "/" + str(amount)
+			
+		Enums.TaskTypeEnum.CatchMouse:
+			time = rng.randf_range(20, 30)
+			item = Enums.ItemTypeEnum.Empty
+			amount = 1
+			label.text = str(amount) + "/" + str(amount)
+			
+		Enums.TaskTypeEnum.PottyPotty:
+			icon.texture = PottyPottyTexture
+			time = rng.randf_range(20, 30)
+			item = Enums.ItemTypeEnum.Empty
+			amount = 1
+			label.text = str(amount) + "/" + str(amount)
+			
+		Enums.TaskTypeEnum.Empty:
+			icon.texture = null
+			time = 0
+			item = Enums.ItemTypeEnum.Empty
+			amount = 0
+			label.text = ""
+	pass
