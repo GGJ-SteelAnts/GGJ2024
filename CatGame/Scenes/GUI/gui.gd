945e4b475321ab2d9364f432d0f6c81f957cda10
Martin Šustr <57565777+masu132@users.noreply.github.com>
2024-01-27 15:58:57 +0100

    added cat needs

--- CatGame/Scenes/GUI/gui.gd
+++ CatGame/Scenes/GUI/gui.gd
@@ -4,12 +4,36 @@ extends Node
 @onready var Task2 = get_node("TextureRect2/Tasks")
 @onready var Task3 = get_node("TextureRect3/Tasks")
 
+@export var NewTaskTime = 20.0
+var ActualTaskTime = 0.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Task1.TaskType = Task1.chooseTask()
+	Task2.TaskType = Task2.chooseTask()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Task1.TaskType == Task1.TaskTypes.Empty && Task2.TaskType != Task2.TaskTypes.Empty:
+		Task1.TaskType = Task2.TaskType
+		Task2.TaskType = Task1.TaskTypes.Empty
+	if Task1.TaskType == Task1.TaskTypes.Empty && Task3.TaskType != Task3.TaskTypes.Empty:
+		Task1.TaskType = Task3.TaskType
+		Task3.TaskType = Task1.TaskTypes.Empty
+	if Task2.TaskType == Task1.TaskTypes.Empty && Task3.TaskType != Task3.TaskTypes.Empty:
+		Task2.TaskType = Task3.TaskType
+		Task3.TaskType = Task1.TaskTypes.Empty
+	
+	ActualTaskTime += delta
+	if ActualTaskTime > NewTaskTime:
+		if Task1.TaskType == Task1.TaskTypes.Empty:
+			Task1.TaskType = Task1.chooseTask()
+		elif Task2.TaskType == Task2.TaskTypes.Empty:
+			Task2.TaskType = Task2.chooseTask()
+		elif Task3.TaskType == Task3.TaskTypes.Empty:
+			Task3.TaskType = Task3.chooseTask()
+		ActualTaskTime = 0.0
 	pass
 
--- CatGame/Scenes/Tasks/tasks.gd
+++ CatGame/Scenes/Tasks/tasks.gd
@@ -1,13 +1,11 @@
 extends Control
 
-enum TaskTypes {Hungry , Sleepy, Catnipy, Shitty}
+enum TaskTypes {Empty, Hungry , Sleepy, Catnipy, Shitty}
 var TaskType : TaskTypes
 @onready var icon = get_node("Icon")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var task = chooseTask()
-	TaskType = task
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,15 +18,15 @@ func chooseArt(task):
 		TaskTypes.Hungry:
 			icon.texture = load("res://Scenes/Assets/Tasks/Hungry.png")
 		TaskTypes.Sleepy:
-			icon.texture = load("res://Scenes/Assets/Tasks/Hungry.png")
+			icon.texture = load("res://Scenes/Assets/Tasks/Sleepy.png")
 		TaskTypes.Catnipy:
-			icon.texture = load("res://Scenes/Assets/Tasks/Shitty.png")
+			icon.texture = load("res://Scenes/Assets/Tasks/Catnipy.png")
 		TaskTypes.Shitty:
 			icon.texture = load("res://Scenes/Assets/Tasks/Shitty.png")
 
 func chooseTask():
 	var rng = RandomNumberGenerator.new()
-	var random_number = rng.randi_range(0, TaskTypes.size() - 1)
+	var random_number = rng.randi_range(1, TaskTypes.size() - 1)
 	var taskString = TaskTypes.keys()[random_number]
 	var task : TaskTypes
 	match taskString:
