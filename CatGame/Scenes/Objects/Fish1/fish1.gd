80dc129064d7ac25d9a50def83010011296701a4
haitem <silentislp@gmail.com>
2024-01-27 02:22:46 +0100

    Push objects script

--- CatGame/Scenes/Objects/Fish1/fish1.gd
+++ CatGame/Scenes/Objects/Fish1/fish1.gd
@@ -2,16 +2,28 @@ extends Node2D
 
 enum ObjectsTypes {Fish,Nothing = -1}
 @export var type = ObjectsTypes.Nothing
+@export var interactable = false
+
 @onready var label = get_node("Label")
 
-func _process(delta):
-	pass
+func _process(_delta):
+	if interactable && Input.is_action_pressed("Interact"):
+		Interaction()
+		interactable = false
 
 func _on_area_2d_body_entered(body):
-	if body.is_in_group("Player"):
+	if body.name == "Player":
 		label.visible = true
+		interactable = true
 
 
 func _on_area_2d_body_exited(body):
-	if body.is_in_group("Player"):
+	if body.name == "Player":
 		label.visible = false
+		interactable = false
+
+func Interaction():
+	if type == ObjectsTypes.Fish:
+		print("action")
+	else:
+		pass
