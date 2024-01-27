19d86ca05af5cb9d2b38b1982a33b294af3499c8
haitem <silentislp@gmail.com>
2024-01-27 02:54:37 +0100

    Modify script for interaction

--- CatGame/Scenes/Objects/Objects.gd
+++ CatGame/Scenes/Objects/Objects.gd
@@ -3,19 +3,25 @@ extends Node2D
 enum ObjectsTypes {Fish,Nothing = -1}
 @export var type = ObjectsTypes.Nothing
 @export var interactable = false
+@export var canInteract = true
+
+@export var spriteNormal : Texture
+@export var spriteAction : Texture
+
+@export var particles : GPUParticles2D
 
 @onready var label = get_node("Label")
+@onready var sprite = get_node("Sprite2D")
 
 func _process(_delta):
-	if interactable && Input.is_action_pressed("Interact"):
+	if interactable && Input.is_action_pressed("Interact") && canInteract:
 		Interaction()
 		interactable = false
 
 func _on_area_2d_body_entered(body):
-	if body.name == "Player":
+	if body.name == "Player" && canInteract:
 		label.visible = true
 		interactable = true
-
 
 func _on_area_2d_body_exited(body):
 	if body.name == "Player":
@@ -24,6 +30,6 @@ func _on_area_2d_body_exited(body):
 
 func Interaction():
 	if type == ObjectsTypes.Fish:
-		print("action")
-	else:
-		pass
+		if spriteAction != null:
+			sprite.texture = spriteAction
+			canInteract = false
