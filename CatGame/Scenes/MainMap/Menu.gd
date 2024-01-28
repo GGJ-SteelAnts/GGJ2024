8644768f2e82f5947dadfa682ab977872902c35a
haitem <silentislp@gmail.com>
2024-01-28 02:03:51 +0100

    main menu

--- CatGame/Scenes/MainMap/Menu.gd
+++ CatGame/Scenes/MainMap/Menu.gd
@@ -2,6 +2,10 @@ extends Control
 
 var map = preload("res://Scenes/MainMap/map.tscn")
 
+@onready var mainMenu = get_node("MainMenu")
+@onready var credits = get_node("Credits")
+@onready var controls = get_node("Controls")
+
 func _on_start_game_pressed():
 	var game = map.instantiate()
 	game.menu = self
@@ -16,3 +20,19 @@ func _on_exit_game_pressed():
 func _input(event):
 	if event.is_action_pressed("Exit") && visible:
 		get_tree().quit()
+
+
+func _on_credits_pressed():
+	mainMenu.hide()
+	credits.show()
+
+
+func _on_controls_pressed():
+	controls.show()
+	mainMenu.hide()
+
+
+func _on_texture_button_pressed():
+	mainMenu.show()
+	controls.hide()
+	credits.hide()
