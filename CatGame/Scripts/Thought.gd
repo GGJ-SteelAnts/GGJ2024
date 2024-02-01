1200b2138cb6235bacde265282432938e2259188
Ernest0012 <ernest0012@seznam.cz>
2024-02-01 09:35:28 +0100

    main map cleanup

--- CatGame/Scripts/Thought.gd
+++ CatGame/Scripts/Thought.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+@export var thoughtGUI : Sprite2D
+
+
+
 var player : Player
 var stats : PlayerStats
 
@@ -9,13 +13,19 @@ var worstStat : Stat
 func _ready():
 	player = get_node("/root/Map/Dynamics/Player")
 	stats = player.playerStats
+	thoughtGUI.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
+# TO DO: implement critical treshold for  each stat
 func EvaluateWorseStat():
-	
+	worstStat = stats["Needs"]
+	for s in stats.values():
+		if s == worstStat:
+			continue
+		if s.Frac() < worstStat.Frac():
+			worstStat = s
 	return
